3c0311d57fba47ea86fb24ab01140c77d1b0e7ac
Harsh Agarwal <harshagarwal7654@gmail.com>
2024-05-25 14:41:20 +0530

    buildings list api

--- src/routes/buildingRoutes.js
+++ src/routes/buildingRoutes.js
@@ -3,9 +3,12 @@ const { validateAuth } = require('../auth/auth');
 const router = express.Router();
 const createBuildingController = require('../controllers/building/createBuilding');
 const createBuildingAdminController = require('../controllers/building/createBuildingAdmin')
+const buildingListController = require('../controllers/building/buildingList')
 
 router.post('/buildingRegistration',  validateAuth, createBuildingController.createBuilding);
 
 router.post('/buildingAdminRegistration',  validateAuth, createBuildingAdminController.createBuildingAdmin);
+
+router.get('/buildingsList', validateAuth, buildingListController.getAllBuildings);
 
 module.exports = router;
